601fe26372af031a193c44b2f66c071ded5f7847
Joseph <jblackl3@uncc.edu>
2019-04-10 03:18:05 +0000

    added more slides to rootpage - added some styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,4 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require turbolinks
 //= require_tree
